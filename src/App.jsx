1ea35004204fff102916307cf1c54c1807b6e24e
Nilotpaul Nandi <nilotpaul.nandi2021@uem.edu.in>
2023-06-07 23:28:26 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+
+function App() {
+
+  return (
+
+        <div className="main">
+        <div className="info">
+          <h2>Join Us Today!</h2>
+          <p className='money'>30-Day Money Back Guarantee</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, sit laboriosam aliquam tempore neque veritatis dolorum laborum rerum odit fugit.</p>
+        </div>
+        <div className="container">
+          <div className="item">
+            <h4>Monthly Subscription</h4>
+            <h2>$69</h2><p className='duration'>per month</p>
+            
+            <p className='benefit'>Lorem ipsum dolor sit.</p>
+            <a href="/"><button>Sign Up</button></a>
+          </div>
+          <div className="item1">
+           <h4>Why Us?</h4>
+           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed vitae dolorum, totam quibusdam dolorem tenetur?</p>
+          </div>
+        </div>
+     
+     </div>
+  )
+}
+
+export default App
